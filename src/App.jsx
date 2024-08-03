88f49f33f7773b7270acd353f18f11c2e55bd449
carlosmacarao <carlosmacarao9@gmail.com>
2024-08-04 00:55:30 +0200

    cleaning the main file

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+      
       <h1>Travel Website</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -25,9 +18,7 @@ function App() {
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat rerum et perspiciatis, est reprehenderit vero eius commodi aut pariatur adipisci aliquid tempore quod exercitationem, laborum quidem blanditiis dolor perferendis ex.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }

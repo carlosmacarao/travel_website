0e4c3e7b9378ef4603763764e0a5f1b3db4a2b4f
carlosmacarao <carlosmacarao9@gmail.com>
2024-08-06 22:25:26 +0200

    new function

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-       
-      <h1>Travel Website</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat rerum et perspiciatis, est reprehenderit vero eius commodi aut pariatur adipisci aliquid tempore quod exercitationem, laborum quidem blanditiis dolor perferendis ex.
-        </p>
-      </div>
-      
+      <h2>Working</h2>
     </>
   )
 }
